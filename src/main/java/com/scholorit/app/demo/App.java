301f84db1faeb48c0e30463438abc036a0310dd5
root <root@DESKTOP-B79IC42>
2023-06-13 04:08:02 -0400

    added pipeline

---
+++ src/main/java/com/scholorit/app/demo/App.java
@@ -0,0 +1,13 @@
+package com.scholorit.app.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
